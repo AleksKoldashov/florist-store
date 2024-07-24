b9407f7dee52ebc2b402424c9f239c86cc9398c1
Aleksey Koldashov <123095853+AleksKoldashov@users.noreply.github.com>
2024-07-24 20:14:16 +0400

    add maket

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,55 @@
 import React from "react"
-
-
+import '../styles/app.css'
+import logo from '../assets/img/logo.svg'
+import flowerbig from '../assets/img/flowerbig.svg'
+import flowersmall from '../assets/img/flowersmall.svg'
+import search from '../assets/img//imgheader/search.svg'
+import basket from '../assets/img//imgheader/basket.svg'
 
 export default function App (){
 
-    return<div>
-    <h1>Welcom!</h1>
+const headerMenu=[
+    {id:1, title: 'Home'},
+    {id:2, title: 'Shop'},
+    {id:3, title: 'Plant Care'},
+    {id:4, title: 'Blogs'},
+]
+
+
+return<div className="app">
+        <div className="header-app">
+            <img src={logo} alt="logo"/>
+            <div className="header-center">
+            {
+                headerMenu.map((item)=><a key={item.id}>{item.title}</a>)
+            }
+            </div>
+            <div className="header-r">
+            <img src={search} alt="logo"/>
+            <img src={basket} alt="logo"/>
+            <button>Login</button>
+            </div>
+        </div>
+        <div className="mainBaner">
+            <div className="textbaner">
+                <h4>Welcome to GreenShop</h4>
+                <h1>Let’s Make a
+                Better Planet</h1>
+                <h5>We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!</h5>
+                <button className="btn-baner">SHOP NOW</button>
+            </div>   
+            <img src={flowerbig} alt="flowerbig" className="flowerbig"/>
+            <img src={flowersmall} alt="flowersmall" className="flowersmall"/>
+        </div>
+        <div className="home-content">
+            <div className="categories">
+                <h3>Categories</h3>
+            </div>
+            <div className="products">
+
+            </div>
+        </div>
+   
+    <h1>Welcom</h1>
     </div>
 }
---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkgyWuklNj-e6zIgQEO-qHDZIa7updot0",
+    authDomain: "florist-project-78b45.firebaseapp.com",
+    projectId: "florist-project-78b45",
+    storageBucket: "florist-project-78b45.appspot.com",
+    messagingSenderId: "125666094034",
+    appId: "1:125666094034:web:3c4a48bf3c18f28c14b60b"
+  };
+
+export const app = initializeApp(firebaseConfig);
+
+
